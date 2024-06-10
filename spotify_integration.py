cae20116ae3ff71b376b2dcc8637de67c302001f
izzahalzahri <nurizzahathirah@yahoo.com>
2024-06-10 19:23:24 +0800

    Update spotify_integration.py

--- spotify_integration.py
+++ spotify_integration.py
@@ -4,7 +4,7 @@ import os
 from dotenv import load_dotenv
 import pandas as pd
 
-load_dotenv(spotify.env)
+load_dotenv()
 
 SPOTIPY_CLIENT_ID = os.getenv('SPOTIPY_CLIENT_ID')
 SPOTIPY_CLIENT_SECRET = os.getenv('SPOTIPY_CLIENT_SECRET')
